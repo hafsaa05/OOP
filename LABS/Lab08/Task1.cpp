e28c89eb441079d27f7fbd5ffd829d9ab4b09319
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2024-03-23 11:10:25 +0500

    Update Task1.cpp

--- LABS/Lab08/Task1.cpp
+++ LABS/Lab08/Task1.cpp
@@ -2,3 +2,31 @@
 Programmer: Hafsa Rashid
 Student ID: 23K-0064
 */
+#include<iostream>
+using namespace std;
+class Book{
+public:
+string title, author, publisher;
+Book(string title, string author, string publisher): title(title), author(author), publisher(publisher) {}
+void display(){
+    cout<<"Title: "<<title<<endl;
+    cout<<"Author: "<<author<<endl;
+    cout<<"Publisher: "<<publisher<<endl;
+    }
+};
+class FictionBook: public Book{
+public:
+string genre, protagonist;
+FictionBook (string title, string author, string publisher, string genre, string protagonist): Book(title, author, publisher), genre(genre), protagonist(protagonist) {}
+void display(){
+    cout<<"Title: "<<title<<endl;
+    cout<<"Author: "<<author<<endl;
+    cout<<"Publisher: "<<publisher<<endl;
+    cout<<"Genre: "<<genre<<endl;
+    cout<<"Protagonist: "<<protagonist<<endl;
+}
+};
+int main() {
+    FictionBook obj("Harry Potter", "J.K Rowling", "mystery", "Fantasy", "Hermione");
+    obj.display();
+}
